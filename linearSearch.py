c0941c677da74b8ac9d4a3ee72512fa3c88fad0c
Sahaj Kulshrestha <31099896+Sahaj21@users.noreply.github.com>
2018-09-26 18:20:11 +0530

    linear search

---
+++ linearSearch.py
@@ -0,0 +1,15 @@
+ans=int(input("Enter number to be searched "));
+print("Enter the array to be searched \n")
+
+
+ar = [int(i) for i in input().split()]
+flag=0;
+for i in range(len(ar)):
+	if ar[i] == ans:
+		flag=flag+1;
+print(flag)
+
+if flag==0:
+	print("The element is not present in the array")
+else:
+	print("The element comes", flag , "times in the array")
